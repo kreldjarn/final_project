8b15a99ce22ba87802dc2dcef6184bcb2d2be96a
Kristján Eldjárn <kreld@kreld.vodafone>
2013-11-07 17:51:27 +0000

    git vesen

--- flashcards/urls.py
+++ flashcards/urls.py
@@ -2,6 +2,6 @@ from django.conf.urls import patterns, url
 from flashcards import views
 
 urlpatterns = patterns('',
-    url(r'^$', views.index, name='index'),
+    url(r'^$', views.view_decks, name='index'),
     url(r'^(?P<deck_id>\d+)/$', views.deck_view, name='deck_view'),
 )
--- flashcards/views.py
+++ flashcards/views.py
@@ -1,8 +1,11 @@
 from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
 from flashcards.models import *
 
-def index(request):
-    return HttpResponse("test")
+def view_decks(request):
+    template_name = "flashcards/view_decks.html"
+    decks = Deck.objects.all()
+    return render(request, template_name, locals())
 
 def deck_view(request, deck_id):
-    decks = Deck.objects.all()
+    return HttpResponse("test %s", deck_id)
