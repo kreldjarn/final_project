265ddacfb9770048fe332e1f218da9f69dce8962
Kristján Eldjárn <kreld@kreld.vodafone>
2013-11-14 15:45:07 +0000

    Reit nýtt fall í bakenda til þess að svara spurningum í view_single_deck

--- flashcards/static/js/selectDeck.js
+++ flashcards/static/js/selectDeck.js
@@ -4,7 +4,7 @@ $('#selectDeck').change(function(){
 		window.location = "/";
 		return;
 	}
-	window.location = "/" + $(this).val();
+	window.location = "/" + $(this).val() + "/";
 });
 // Ef við erum að skoða bunka, veljum við nafn bunkans í select-tagginu.
 if (deck) $('#selectDeck').val(deck.id);
--- flashcards/urls.py
+++ flashcards/urls.py
@@ -4,8 +4,6 @@ from .views import view_decks, deck, right, wrong, create_deck, create_cards
 urlpatterns = patterns('',
     url(r'^$', view_decks.as_view(), name='index'),
     url(r'^(?P<deck_id>\d+)/$', deck.as_view(), name='deck_view'),
-    url(r'^(?P<card_id>\d+)/rett/$', right, name='right'),
-    url(r'^(?P<card_id>\d+)/rangt/$', wrong, name='wrong'),
     url(r'^create/$', create_deck.as_view(), name='create_deck'),
     url(r'^create/(?P<deck_id>\d+)/', create_cards.as_view(), name="create_card"),
 )
