f55df9e9f371811d5342ec550515e41602c7d786
Halldór Eldjárn <halldor@admittor.com>
2013-11-16 13:28:30 +0000

    Bug fixes, JS templating engine
    Fixed bug where buttons would not work due to CSS3 transform.
    Stack icons still need their CSS overhaul.
    Incorporated a client-side templating engine for displaying the cards, as we want to mutate the order of them without doing redundant DOM gymnastics.

--- flashcards/static/js/view_single_deck.js
+++ flashcards/static/js/view_single_deck.js
@@ -27,6 +27,33 @@ top_el.delegate(".answer button", "click", function(e) {
     });
 });
 
+function showCards()
+{
+    var html = "";
+    var template = $('#flip-card').html();
+    var compiled = _.template(template);
+    for(var i = 0; i < cards.length; ++i)
+    {
+        var c = cards[i];
+        html += compiled({
+            id: c.id,
+            question: c.question,
+            answer: c.answer
+        });
+    }
+
+    $("section#cards").html(html);
+}
+
+
+// Færum þetta e-ð annað. Í raun til að rendera
+// template-ið og spýta út í DOM ið eftir því hvernig
+// röðin á kortunum eru í cards[] JS fylkinu
+// S.s. til að update-a viewið fyrir læri-ham.
+// Því við viljum geta shufflað/bætt aftast í random röð
+// og svo framvegis, þegar líður á lærdóms-sessjón.
+showCards();
+
 function updateCardView(card_el, ans_value)
 {
     var cards_top_el = $("section#cards");
